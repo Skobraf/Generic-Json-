605f5666ae87ffcc141ed02ce5d130989b092e22
skobraf <skobraf@gmail.com>
2019-02-21 13:34:37 +0000

    intercation with dom working

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 class App extends React.Component {
+	statusRef = React.createRef();
 	state = {
 		section:{}
+	}
+	checkValue = (e) => {
+		e.preventDefault();
+		const valeur = this.statusRef.current.value;
+		console.log(valeur);
 	}
 	addValue = () => {
 		const section = {...this.state.section};
 		section[`x${Date.now()}`] = new Boolean;
-		this.setState({ section })
-		console.log(this.state)
+		this.setState({ section });
 	}
 	render() {
 		return (
 			<div>
-				<form>
+				<form onSubmit={this.checkValue}>
 					<input type="text" placeholder="Text"/>
-					<select name="" id="">
-					<option value="">text</option>
-					<option value="">boolean</option>
+					<select name="valeur" ref={this.statusRef}>
+					<option value="text">text</option>
+					<option value="boolean">boolean</option>
 					</select>
+					<button type="submit"> click me</button>
 				</form>
 			</div>
 			)
